cfbdd78db2e4aec8e7f337e70bf4a8100c4f874f
Dm1ttry <dimagaponov78@gmail.com>
2023-08-22 00:38:27 +0300

    fix

--- features/step-definitions/steps.ts
+++ features/step-definitions/steps.ts
@@ -1,58 +1,58 @@
-import { Given, When, Then } from '@wdio/cucumber-framework';
-import { expect, $ , browser} from '@wdio/globals'
-import Page from '../pageobjects/page.js';
-import LoginPage from '../pageobjects/login.page.js';
-import { logger } from '../../log.config.js';
-
+import { Given, When, Then } from "@wdio/cucumber-framework";
+import { expect, $, browser } from "@wdio/globals";
+import Page from "../pageobjects/page.js";
+import LoginPage from "../pageobjects/login.page.js";
+import { logger } from "../../log.config.js";
 
 const login = new LoginPage();
 const page = new Page();
 
 Given(/^I open the email login page/, async () => {
-    await login.openLogin()
-    logger.info("Open page")
+    await login.openLogin();
+    logger.info("Open page");
 });
 
-When(/^I input email and click login/ , async () => {
-    await  login.loginEmail("12456dawdwa");
-    logger.info("Login")
+When(/^I input email and click login/, async () => {
+    await login.loginEmail("12456dawdwa");
+    logger.info("Login");
 });
 Then(/^I should see an error/, async () => {
-    await expect(login.ErrorLogin).toBeDisplayed
+    await expect(login.ErrorLogin).toBeDisplayed;
 });
 
 Given(/^I open the phone login page/, async () => {
-    await login.openLogin()
+    await login.openLogin();
 });
-When(/^I input phone and click login/ , async () => {
-    await  login.loginPhone("+30121546452466555");
+When(/^I input phone and click login/, async () => {
+    await login.loginPhone("+30121546452466555");
 });
 Then(/^I should see form error/, async () => {
-    await expect(login.ErrorPhoneForm).toBeDisplayed
-})
- 
-Given(/^I open Kinopoisk/, async () => {
-    await page.openKinopoisk()
-    logger.info("Open Kinopoisk")
-    
+    await expect(login.ErrorPhoneForm).toBeDisplayed;
 });
-When(/^l scroll and cliclk on movie/ , async () => {
-    await browser.scroll(0, 700)
-    await page.OpenMovie()
-    logger.info("Open Movie page")
+
+Given(/^I open Kinopoisk/, async () => {
+    await page.openKinopoisk();
+    logger.info("Open Kinopoisk");
+});
+When(/^l scroll and cliclk on movie/, async () => {
+    await browser.scroll(0, 800);
+    await page.OpenMovie();
+    logger.info("Open Movie page");
 });
 Then(/^I see page movie/, async () => {
-    await expect(page.NameMovie).toHaveText("Два, три, демон, приди! (2022)")
-    logger.info("Movie page")
-})
+    await expect(page.NameMovie).toHaveText("Два, три, демон, приди! (2022)");
+    logger.info("Movie page");
+});
 
 Given(/^I am open Kinopoisk/, async () => {
-    await page.openKinopoisk()
+    await page.openKinopoisk();
 });
-When(/^I scroll and open article/ , async () => {
-    await browser.scroll(0, 1500)
-    await  page.ArticleClick()
+When(/^I scroll and open article/, async () => {
+    await browser.scroll(0, 2000);
+    await page.ArticleClick();
 });
 Then(/^I see article/, async () => {
-    await expect(browser).toHaveUrl("https://www.kinopoisk.ru/media/article/4008240/")
-})
+    await expect(browser).toHaveUrl(
+        "https://www.kinopoisk.ru/media/article/4008070/"
+    );
+});
